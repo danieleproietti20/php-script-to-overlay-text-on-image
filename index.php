e48ffaad018ea17d2f422beb1f5a333be644882e
daniele <daniele.proietti2020@gmail.com>
2020-06-21 18:55:59 -0700

    chores: wipe functions

--- index.php
+++ index.php
@@ -1,134 +1,126 @@
 <?php
-	// Stage 1: Replicate the first image style
-	// Stage 2: Replicate the first image style, with the user able to select different background images.
-	// Stage 3: Introduce a second image style, with different background images.
-	if (isset($_POST['submit'])) {
+// Stage 1: Replicate the first image style
+// Stage 2: Replicate the first image style, with the user able to select different background images.
+// Stage 3: Introduce a second image style, with different background images.
 
+$textSpacing = 30;
+$text = "LANUNCHING SOON!";
+$text2 = "SUBSCRIBE TO UPDATES";
 
-		$file_name = $_POST['image'];
-			// $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+$our_image;
+$height;
+$width;
+$top;
+$top2;
+$marginLeft = 20;
 
+if (isset($_POST['submit'])) {
 
-		header('Content-type: image/png');
+    $file_name = $_POST['image'];
 
-		// Load And Create Image From Source
-		$our_image = imagecreatefrompng("./images/".$file_name);
-		
-		// $red = imagecolorallocate($our_image, 255, 0, 0);
-		// imagefilledrectangle($our_image, 4, 4, 50, 25, $red);
-		
-		
-		// $color = imagecolorallocatealpha($our_image, 255, 255, 255, 127);
-		// imagefill($our_image, 0, 0, $color);
-		
-		// // Allocate A Color For The Text Enter RGB Value
-		
-		$white = imagecolorallocatealpha($our_image, 0, 0, 0, 60);
-		
-		$height = imagesy($our_image);
-		$width = imagesx($our_image);
-		
-		imagefilledrectangle($our_image, 0, $height/2, $width, $height, $white);
-		
-		
-		$white_color = imagecolorallocate($our_image, 255, 255, 255);
-		
-		// Set Path to Font File
-		$font_path = __DIR__.'/D-DINCondensed-Bold.otf';
-		
-		// Set Text to Be Printed On Image
-		$text ="LANUNCHING SOON!";
-		
-		$fontSize=24;
-		$angle=0;
-		$left=125;
-		$marginLeft = 20;
-		$marginTop = 50;
-		$top = $height/2 + $marginTop;
-		$textSpacing = 30;
-		
-		$dimensions = imagettfbbox($fontSize, $angle, $font_path, $text);
-		$textWidth = abs($dimensions[4] - $dimensions[0]);
-		$left = imagesx($our_image) - $textWidth - $marginLeft;
-		
-		// Print Text On Image
-		imagettftext($our_image, $fontSize, $angle, $left, $top, $white_color, $font_path, $text);
+    header('Content-type: image/png');
 
+    // Load And Create Image From Source
+    $GLOBALS['our_image'] = imagecreatefrompng("./images/" . $file_name);
 
-		$text2 = "SUBSCRIBE TO UPDATES";
-		$top2 = $top + imagefontheight(32) + $textSpacing;
-		$fontSize2 = 32;
+    function drawRectangle()
+    {
+        $bgcolor = imagecolorallocatealpha($GLOBALS['our_image'], 0, 0, 0, 60);
 
-		$dimensions2 = imagettfbbox($fontSize2, $angle, $font_path, $text2);
-		$textWidth2 = abs($dimensions2[4] - $dimensions2[0]);
-		$left2 = imagesx($our_image) - $textWidth2 - $marginLeft;
+        $GLOBALS['height'] = imagesy($GLOBALS['our_image']);
+        $GLOBALS['width'] = imagesx($GLOBALS['our_image']);
 
+        imagefilledrectangle($GLOBALS['our_image'], 0, $GLOBALS['height'] / 2, $GLOBALS['width'], $GLOBALS['height'], $bgcolor);
+    }
 
+    function drawTextOnImage()
+    {
+        $white_color = imagecolorallocate($GLOBALS['our_image'], 255, 255, 255);
 
-		// Print Second Text On Image
-		imagettftext($our_image, $fontSize2, $angle, $left2, $top2, $white_color, $font_path, $text2);
+        // Set Path to Font File
+        $font_path = __DIR__ . '/D-DINCondensed-Bold.otf';
 
+        // Set Text to Be Printed On Image
+        $fontSize = 24;
+        $angle = 0;
+        $left = 125;
+        $marginTop = 50;
+        $top = $GLOBALS['height'] / 2 + $marginTop;
 
-		$origin_logo_image = imagecreatefromjpeg("./logo.jpeg");
-		$logo_image = imagecreatetruecolor(imagesx($origin_logo_image)/2, imagesy($origin_logo_image)/2);
+        $dimensions = imagettfbbox($fontSize, $angle, $font_path, $GLOBALS['text']);
+        $textWidth = abs($dimensions[4] - $dimensions[0]);
+        $left = imagesx($GLOBALS['our_image']) - $textWidth - $GLOBALS['marginLeft'];
 
-		imageCopyResampled($logo_image, $origin_logo_image, 0, 0, 0, 0, imagesx($origin_logo_image)/2, imagesy($origin_logo_image)/2, imagesx($origin_logo_image), imagesy($origin_logo_image));
+        // Print Text On Image
+        imagettftext($GLOBALS['our_image'], $fontSize, $angle, $left, $top, $white_color, $font_path, $GLOBALS['text']);
 
-		$logo_x = $width - (imagesx($logo_image) + $marginLeft);
-		$logo_y = $top2 + $textSpacing;
-		imagecopy(
-			$our_image, $logo_image,
-			$logo_x,
-			$logo_y,
-			0, 
-			0, 
-			imagesx($logo_image), imagesy($logo_image)
-		);
+        $GLOBALS['top2'] = $top + imagefontheight(32) + $GLOBALS['textSpacing'];
+        $fontSize2 = 32;
 
-		// Send Image to Browser
-		imagepng($our_image);
-		
-		// Clear Memory
-		imagedestroy($our_image);
-		
+        $dimensions2 = imagettfbbox($fontSize2, $angle, $font_path, $GLOBALS['text2']);
+        $textWidth2 = abs($dimensions2[4] - $dimensions2[0]);
+        $left2 = imagesx($GLOBALS['our_image']) - $textWidth2 - $GLOBALS['marginLeft'];
 
-		// function generateRect() {
-		//     $white = imagecolorallocatealpha($our_image, 255, 255, 255, 60);
-		//     $height = imagesy($our_image);
-		//     $width = imagesx($our_image);
-		
-		//     imagefilledrectangle($our_image, 0, $height/2, $width, $height, $white);
-		// }
+        // Print Second Text On Image
+        imagettftext($GLOBALS['our_image'], $fontSize2, $angle, $left2, $GLOBALS['top2'], $white_color, $font_path, $GLOBALS['text2']);
+    }
 
-	}
+    function drawLogoOnImage()
+    {
+        $origin_logo_image = imagecreatefromjpeg("./logo.jpeg");
+        $logo_image = imagecreatetruecolor(imagesx($origin_logo_image) / 2, imagesy($origin_logo_image) / 2);
+
+        imageCopyResampled($logo_image, $origin_logo_image, 0, 0, 0, 0, imagesx($origin_logo_image) / 2, imagesy($origin_logo_image) / 2, imagesx($origin_logo_image), imagesy($origin_logo_image));
+
+        $logo_x = $GLOBALS['width'] - (imagesx($logo_image) + $GLOBALS['marginLeft']);
+        $logo_y = $GLOBALS['top2'] + $GLOBALS['textSpacing'];
+        imagecopy(
+            $GLOBALS['our_image'], $logo_image,
+            $logo_x,
+            $logo_y,
+            0,
+            0,
+            imagesx($logo_image),
+            imagesy($logo_image)
+        );
+        imagedestroy($logo_image);
+        imagedestroy($origin_logo_image);
+    }
+
+    drawRectangle();
+    drawTextOnImage();
+    drawLogoOnImage();
+
+    // Send Image to Browser
+    imagepng($our_image);
+
+    // Clear Memory
+    imagedestroy($our_image);
+
+}
 ?>
 <html>
    <body>
-      
+
    <form method="post" enctype="multipart/form-data" >
     <div>
 			<img src="test.png" alt="Girl in a jacket" id="test1" style="width: 80px; height: 80px;">
 			<div class="checkbox">
-
 				<label for="test1">
 					<input type="checkbox" value="test.png" name="image">
 				</label>
-				
 			</div>
 		</div>
 		<div>
 			<img src="test.png" alt="Girl in a jacket" id="test2" style="width: 80px; height: 80px;">
-
 			<div class="checkbox">
-
 				<label for="test2">
 					<input type="checkbox" value="test.png" name="image">
 				</label>
-
 			</div>
 		</div>
 		<button type="submit" name="submit">Submit</button>
 	</form>
-      
+
    </body>
 </html>
